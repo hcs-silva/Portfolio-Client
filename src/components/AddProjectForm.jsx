c7b2e412d51888720fa3d255296eec670a32fd5a
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-03-01 10:43:08 +0000

    added the logic for adding a project to the portfolio

--- src/components/AddProjectForm.jsx
+++ src/components/AddProjectForm.jsx
@@ -1,57 +1,88 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import gitHubstd from "../assets/github-mark.svg";
 import gitHubWhi from "../assets/github-mark-white.svg";
-import css from "../assets/css-icon.svg"
+import css from "../assets/css-icon.svg";
 import html from "../assets/html-icon.svg";
 import js from "../assets/javascript.svg";
 import reactIcon from "../assets/react-js-icon.svg";
 import nodeIcon from "../assets/node-js-icon.svg";
 import mongodb from "../assets/mongodb-icon.svg";
 import Footer from "./Footer";
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5005";
+const webToken = localStorage.getItem("authToken");
 
 const AddProjectForm = () => {
   const [projectTitle, setProjectTitle] = useState("");
   const [projectThumbnail, setProjectThumbnail] = useState("");
-  const [collaboratorInput, setCollaboratorInput] = useState("");
+  const [collaborators, setCollaborators] = useState("");
   const [technologies, setTechnologies] = useState("");
   const [github, setGithub] = useState("");
   const [liveLink, setLiveLink] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
   const [imageFile, setImageFile] = useState(null); // File state
   const [uploading, setUploading] = useState("");
+  const [projectDate, setProjectDate] = useState("");
 
-  function handleSubmit(e) {
+  const nav = useNavigate()
+
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    const newCollaborators = collaboratorInput
+    const newCollaborators = collaborators
       .split(",")
       .map((name) => name.trim()) // Remove spaces around names
       .filter((name) => name !== ""); // Remove empty strings
+
+    const newTechnologies = technologies
+      .split(",")
+      .map((tech) => tech.trim())
+      .filter((tech) => tech !== "");
 
     const newProject = {
       title: projectTitle,
       thumbnail: projectThumbnail,
       collaborators: newCollaborators,
-      github: github,
+      technologies: newTechnologies,
+      githubLink: github,
       liveLink: liveLink,
       description: projectDescription,
+      date: projectDate,
     };
+
+    console.log(newProject)
+
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/projects/`,
+        newProject,
+        { headers: { authorization: `${webToken}` } }
+      );
+      if(response) {
+        alert("Project Added Sucessfully")
+        setProjectTitle("");
+        setProjectThumbnail("");
+        setCollaborators("");
+        setTechnologies("");
+        setGithub("");
+        setLiveLink("");
+        setProjectDescription("");
+        setImageFile(null);
+        setUploading("");
+        setProjectDate("");
+
+        nav("/dashboard")
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function handleImageChange(e) {
     setImageFile(e.target.files[0]);
   }
-
-  // Function to add multiple collaborators at once
-  const addCollaborators = () => {
-    if (collaboratorInput.trim() !== "") {
-      // Split input by commas, remove spaces, and filter out empty values
-
-      setCollaborators([...collaborators, ...newCollaborators]);
-      setCollaboratorInput(""); // Clear input after adding
-    }
-  };
 
   const handleImageUpload = async () => {
     if (!imageFile) return;
@@ -103,6 +134,26 @@ const AddProjectForm = () => {
             ></textarea>
           </label>
           <label>
+            Technologies Used:
+            <input
+              type="text"
+              value={technologies}
+              onChange={(e) => {
+                setTechnologies(e.target.value);
+              }}
+            />
+          </label>
+          <label>
+            Collaborators:
+            <input
+              type="text"
+              value={collaborators}
+              onChange={(e) => {
+                setCollaborators(e.target.value);
+              }}
+            />
+          </label>
+          <label>
             GitHub Link:
             <input
               type="url"
@@ -121,18 +172,8 @@ const AddProjectForm = () => {
             />
           </label>
           <label>
-            Collaborators:
-            <input
-              type="text"
-              value={collaboratorInput}
-              onChange={(e) => {
-                setCollaboratorInput(e.target.value);
-              }}
-            />
-          </label>
-          <label>
             Date of Project:
-            <input type="date" onChange={(e) => setAquired(e.target.value)} />
+            <input type="text" onChange={(e) => setProjectDate(e.target.value)} />
           </label>
           <button
             type="button"
@@ -148,7 +189,7 @@ const AddProjectForm = () => {
       <Link to="/dashboard" className="back-to-dash">
         Back to Dashboard
       </Link>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
