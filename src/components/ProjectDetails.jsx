6e448c5db234f97d450d36121cbac742b815b597
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-03-02 14:53:40 +0000

    improvements on the project detail cards

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -4,7 +4,14 @@ import { AuthContext } from "../contexts/AuthContext";
 import { Link, useParams } from "react-router-dom";
 import github from "../assets/github-mark-white.svg";
 import globe from "../assets/globe-svgrepo-com.svg";
-import { DiJavascript , DiReact, DiHtml5, DiCss3, DiNodejs, DiMongodb} from "react-icons/di";
+import {
+  DiJavascript,
+  DiReact,
+  DiHtml5,
+  DiCss3,
+  DiNodejs,
+  DiMongodb,
+} from "react-icons/di";
 import { SiExpress } from "react-icons/si";
 import { IoLogoGithub } from "react-icons/io";
 import { CiGlobe } from "react-icons/ci";
@@ -39,21 +46,20 @@ const ProjectDetails = () => {
     }
   }, [projectId]);
 
-  const icons = {
-    javascript: <DiJavascript />,
-    react: <DiReact />,
-    html: <DiHtml5 />,
-    css: <DiCss3 />,
-    node: <DiNodejs />,
-    mongodb: <DiMongodb />,
-    express: <SiExpress />,
-    github: <IoLogoGithub />,
-    globe: <CiGlobe />,
+  const techIcons = {
+    javascript: <DiJavascript size="2em"/>,
+    react: <DiReact size="2em"/>,
+    html: <DiHtml5 size="2em"/>,
+    css: <DiCss3 size="2em"/>,
+    node: <DiNodejs size="2em"/>,
+    mongodb: <DiMongodb size="2em"/>,
+    express: <SiExpress size="2em"/>,
+    github: <IoLogoGithub size="2em"/>,
+    globe: <CiGlobe size="2em"/>,
   };
 
   return (
-   
-      <div className="projectDetail">
+    <div className="projectDetail">
       <h1>Project Details</h1>
       {oneProject ? (
         <div className="project-detail-card">
@@ -62,27 +68,58 @@ const ProjectDetails = () => {
             <img src={oneProject.thumbnail} alt="Project Thumbnail" />
           </div>
           <div className="bottom-detail">
-            <p> <em>Description:</em> {oneProject.description}</p>
-            <p><em>Collaborators: </em>
+            <p>
+              {" "}
+              <em>Description:</em> {oneProject.description}
+            </p>
+            <p>
+              <em>Collaborators: </em>
               {oneProject.collaborators.map((collaborator, index) => (
                 <span key={index}>{collaborator}</span>
               ))}
             </p>
-            <p> <em>Technologies Used:</em> {oneProject.technologies.map((tech, index) => (
-                <span key={index}>{tech}</span>
-            ))}</p>
+            <p>
+              {" "}
+              <em>Technologies Used:</em>{" "}
+              {oneProject.technologies.map((tech, index) => (
+                <span
+                  key={index}
+                  style={{
+                    marginRight: "1px",
+                    display: "inline-flex",
+                    alignItems: "center",
+                  }}
+                >
+                  {techIcons[tech] ? techIcons[tech] : null}
+                  <span style={{ marginLeft: "5px" }}>{tech}</span>
+                </span>
+              ))}
+            </p>
             <div className="links">
-            <a href={oneProject.githubLink} target="_blank" rel="noopener noreferrer"><IoLogoGithub/></a>
-            <a href={oneProject.liveLink} target="_blank" rel="noopener noreferrer"><CiGlobe/></a>
+              <a
+                href={oneProject.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IoLogoGithub />
+              </a>
+              <a
+                href={oneProject.liveLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <CiGlobe />
+              </a>
             </div>
           </div>
         </div>
       ) : (
         <div>Loading Project...</div>
       )}
-    <Link to="/dashboard" id="dashboard">Back to Dashboard</Link>
+      <Link to="/dashboard" id="dashboard">
+        Back to Dashboard
+      </Link>
     </div>
-    
   );
 };
 export default ProjectDetails;
